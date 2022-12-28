8aeea06899071c44d3642841a668ccd60b4e86ec
Pamu_Wick <59146968+Pamu-WIck@users.noreply.github.com>
2022-12-28 19:45:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ vid = cv2.VideoCapture("./Video/mili.mp4")
 currentFrame = 0
 length = int(vid.get(cv2.CAP_PROP_FRAME_COUNT))
 
-token = 'EAAHyLBJeZB38BAGqOvUlMQBWSkDrkanaUSSxMleHb55LgJ1jeFemBtZBcfN7ZATijruFNrQsoQzCZAjdROsfPrfqK2mQNlwfMiPQNjkjF4GsZBd839ZCM8FsaSwY9iZACw1MS4rqXrzOyMapSMHojZCAike5sFbe12GONXR8mewxKgISMSgchkm2whcNhEHYWmCVrnsOBKdGRQZDZD'
+token = 'EAAHyLBJeZB38BAJiSUK7foFdFAQ2K0tKFjkkxJupTb7xdfmR6sZAGTAfBcXv1QG7GnEROVWRFXRB4kzHJ8lnSLW6xP8qkONXYAgXKZAnWGXYJPWp6ZCR0ZB938k4Ub9JHAkAeOMkNEatzJqQrXyEg2M9VtqyeZAwOD0ZCpXiNzMqyCshYvFsrrZA'
 fb = facebook.GraphAPI(access_token=token)
 
 if os.path.isdir('./frames'):
