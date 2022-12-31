182f0ebbe0c5b0b5203e19e7e9c2add1e4bca5e0
Pamu-Wick <pamudithawm@gmail.com>
2022-12-31 11:04:09 +0530

    No

--- main.py
+++ main.py
@@ -5,39 +5,48 @@ import cv2
 import os
 import You2Dow
 
-link = input("Enter the YouTube video URL: ")
 
-vidPath = './Video'
-for f in os.listdir(vidPath):
-    os.remove(os.path.join(vidPath, f))
+playlist = ["https://www.youtube.com/watch?v=cdwal5Kw3Fc",
+            "https://www.youtube.com/watch?v=09y7JqE-V1w",
+            "https://www.youtube.com/watch?v=a3HZ8S2H-GQ"]
 
-You2Dow.Download(link)
-yt = You2Dow.YouTube(link)
-title = yt.title
+for x in playlist:
+    link = x
 
-vid = cv2.VideoCapture("./Video/" + title + ".mp4")
-currentFrame = 1
-length = int(vid.get(cv2.CAP_PROP_FRAME_COUNT))
+    # vidPath = './Video'
+    # for f in os.listdir(vidPath):
+    #     os.remove(os.path.join(vidPath, f))
 
-token = 'EAAHyLBJeZB38BAB7jLM96FKYB0bm4q3vHCqxIiS2itOgKG1VHZCdxpvoyPMp4OeqipiTh4r4Gd86LGb8p4GnE62DADkuK11xRMqZB1GoBtZBZBmnlhC9kedyfUTk17k16GhVRkMC6DjxpJ2SVvZBcol4L60HuK8Phvq8ZCa3x4KCGMUAKzzl8OD'
-fb = facebook.GraphAPI(access_token=token)
+    You2Dow.Download(link)
+    yt = You2Dow.YouTube(link)
+    title = yt.title
 
-if not os.path.isdir('./frames'):
-    os.mkdir('./frames')
+    vid = cv2.VideoCapture("./Video/" + title + ".mp4")
+    currentFrame = 1
+    length = int(vid.get(cv2.CAP_PROP_FRAME_COUNT))
 
-while currentFrame <= length:
+    token = 'EAAHyLBJeZB38BAB7jLM96FKYB0bm4q3vHCqxIiS2itOgKG1VHZCdxpvoyPMp4OeqipiTh4r4Gd86LGb8p4GnE62DADkuK11xRMqZB1GoBtZBZBmnlhC9kedyfUTk17k16GhVRkMC6DjxpJ2SVvZBcol4L60HuK8Phvq8ZCa3x4KCGMUAKzzl8OD'
+    fb = facebook.GraphAPI(access_token=token)
 
-    success, frame = vid.read()
-    imagePath = './frames/' + str(currentFrame) + '.jpg'
-    cv2.imwrite(imagePath, frame)
-    image = open(imagePath, 'rb')
-    fb.put_photo(image=image, message=title + " " + str(currentFrame) + " out of " + str(length))
-    print("Uploaded " + str(currentFrame))
-    time.sleep(1)
+    if not os.path.isdir('./frames'):
+        os.mkdir('./frames')
 
-    if os.path.isfile('./frames/' + str(currentFrame - 1) + '.jpg'):
-        os.remove('./frames/' + str(currentFrame - 1) + '.jpg')
+    while currentFrame <= length:
 
-    currentFrame += 1
+        success, frame = vid.read()
+        imagePath = './frames/' + str(currentFrame) + '.jpg'
+        cv2.imwrite(imagePath, frame)
+        image = open(imagePath, 'rb')
+        fb.put_photo(image=image, message=title + " " + str(currentFrame) + " out of " + str(length))
+        print("Uploaded " + str(currentFrame))
+        time.sleep(1)
 
-# link = input("Enter the YouTube video URL: ")
+        if os.path.isfile('./frames/' + str(currentFrame - 1) + '.jpg'):
+            os.remove('./frames/' + str(currentFrame - 1) + '.jpg')
+
+        if currentFrame == 3:
+            break
+        else:
+            currentFrame += 1
+
+time.sleep(86400)
