df60eaa553c29bb880edb367ddd4606d444b8b03
Pamu-Wick <pamudithawm@gmail.com>
2022-12-31 14:36:04 +0530

    No

--- main.py
+++ main.py
@@ -5,48 +5,36 @@ import cv2
 import os
 import You2Dow
 
+link = "https://www.youtube.com/watch?v=M9dVG82QynM&ab_channel=IRAJ"
+You2Dow.Download(link)
+yt = You2Dow.YouTube(link)
+title = yt.title
 
-playlist = ["https://www.youtube.com/watch?v=cdwal5Kw3Fc",
-            "https://www.youtube.com/watch?v=09y7JqE-V1w",
-            "https://www.youtube.com/watch?v=a3HZ8S2H-GQ"]
+vid = cv2.VideoCapture("./Video/" + title + ".mp4")
+currentFrame = 1
+fake = 1
+length = int(vid.get(cv2.CAP_PROP_FRAME_COUNT))
 
-for x in playlist:
-    link = x
+token = 'EAAHyLBJeZB38BAB7jLM96FKYB0bm4q3vHCqxIiS2itOgKG1VHZCdxpvoyPMp4OeqipiTh4r4Gd86LGb8p4GnE62DADkuK11xRMqZB1GoBtZBZBmnlhC9kedyfUTk17k16GhVRkMC6DjxpJ2SVvZBcol4L60HuK8Phvq8ZCa3x4KCGMUAKzzl8OD'
+fb = facebook.GraphAPI(access_token=token)
 
-    # vidPath = './Video'
-    # for f in os.listdir(vidPath):
-    #     os.remove(os.path.join(vidPath, f))
+if not os.path.isdir('./frames'):
+    os.mkdir('./frames')
 
-    You2Dow.Download(link)
-    yt = You2Dow.YouTube(link)
-    title = yt.title
+while currentFrame < length:
 
-    vid = cv2.VideoCapture("./Video/" + title + ".mp4")
-    currentFrame = 1
-    length = int(vid.get(cv2.CAP_PROP_FRAME_COUNT))
+    success, frame = vid.read()
+    imagePath = './frames/' + str(currentFrame) + '.jpg'
+    cv2.imwrite(imagePath, frame)
+    image = open(imagePath, 'rb')
+    fb.put_photo(image=image, message=title + " " + str(fake) + " out of " + str(length//2))
+    fake += 1
+    print("Uploaded " + str(currentFrame))
+    time.sleep(0.5)
 
-    token = 'EAAHyLBJeZB38BAB7jLM96FKYB0bm4q3vHCqxIiS2itOgKG1VHZCdxpvoyPMp4OeqipiTh4r4Gd86LGb8p4GnE62DADkuK11xRMqZB1GoBtZBZBmnlhC9kedyfUTk17k16GhVRkMC6DjxpJ2SVvZBcol4L60HuK8Phvq8ZCa3x4KCGMUAKzzl8OD'
-    fb = facebook.GraphAPI(access_token=token)
+    if os.path.isfile('./frames/' + str(currentFrame - 2) + '.jpg'):
+        os.remove('./frames/' + str(currentFrame - 2) + '.jpg')
 
-    if not os.path.isdir('./frames'):
-        os.mkdir('./frames')
-
-    while currentFrame <= length:
-
-        success, frame = vid.read()
-        imagePath = './frames/' + str(currentFrame) + '.jpg'
-        cv2.imwrite(imagePath, frame)
-        image = open(imagePath, 'rb')
-        fb.put_photo(image=image, message=title + " " + str(currentFrame) + " out of " + str(length))
-        print("Uploaded " + str(currentFrame))
-        time.sleep(1)
-
-        if os.path.isfile('./frames/' + str(currentFrame - 1) + '.jpg'):
-            os.remove('./frames/' + str(currentFrame - 1) + '.jpg')
-
-        if currentFrame == 3:
-            break
-        else:
-            currentFrame += 1
+    currentFrame += 2
 
 time.sleep(86400)
